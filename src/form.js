634f2afbf6cf937fe55f9fc34e9bcaa4b285164b
Gabriel Nunes <gabrielnunesdev@outlook.com>
2025-05-22 15:16:58 -0300

    feat: enhance scheduling form with close button, hour selection, and responsive styles

--- src/form.js
+++ src/form.js
@@ -4,6 +4,9 @@ const btnSchedule = document.getElementById("btn-schedule");
 const form = document.querySelector("form");
 const mainContainer = document.querySelector(".main-container");
 
+const hourBtn = document.querySelector("#hour-btn");
+const closeBtn = document.querySelector(".close-btn");
+
 btnNew.addEventListener("click", () => {
   modal.showModal();
   mainContainer.classList.toggle("blur");
@@ -11,6 +14,7 @@ btnNew.addEventListener("click", () => {
 
 btnSchedule.addEventListener("click", () => {
   setTimeout(() => {
+    alert("Agendamento realizado com sucesso!");
     modal.close();
     mainContainer.classList.toggle("blur");
   }, 1000);
@@ -21,4 +25,19 @@ document.addEventListener("keyup", (event) => {
     modal.close();
     mainContainer.classList.remove("blur");
   }
+});
+
+const list = document.querySelector(".hidden");
+
+hourBtn.addEventListener("click", () => {
+  if (list.style.display === "none") {
+    list.style.display = "block"; // Mostrar a lista
+  } else {
+    list.style.display = "none"; // Ocultar a lista
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.close();
+  mainContainer.classList.toggle("blur");
 });
--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,5 +1,8 @@
 import dayjs from "dayjs";
+import {hoursClick} from "../form/hours-click.js";
 import { openingHours } from "../../utils/opening-hours.js";
+
+const hours = document.getElementById("hour-list");
 
 export function hoursLoad({ date }) {
   const opening = openingHours.map((hour) => {
@@ -19,5 +22,27 @@ export function hoursLoad({ date }) {
   //Renderiza os horários
   opening.forEach(({ hour, available }) => {
     const li = document.createElement("li");
+    li.classList.add(available ? "hour-available" : "hour-unavailable");
+    li.textContent = hour;
+
+    if (hour === "09:00") {
+      hourHeaderAdd("Manhã");
+    } else if (hour === "13:00") {
+      hourHeaderAdd("Tarde");
+    } else if (hour === "18:00") {
+      hourHeaderAdd("Noite");
+    }
+    hours.append(li);
   });
+
+  //Adiciona o evento de clique nos horários disponíveis.
+  hoursClick();
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement("li");
+  header.classList.add("hour-period");
+  header.textContent = title;
+
+  hours.append(header);
 }
